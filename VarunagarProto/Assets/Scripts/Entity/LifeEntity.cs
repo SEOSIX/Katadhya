555bc4a5061de71cd7a45cb6ff2bb9b40dcbaee7
SEO <seotoons@gmail.com>
2025-04-01 16:56:05 +0200

    AI Début

--- VarunagarProto/Assets/Scripts/Entity/LifeEntity.cs
+++ VarunagarProto/Assets/Scripts/Entity/LifeEntity.cs
@@ -8,6 +8,7 @@ public class LifeEntity : MonoBehaviour
       [Header("Enemy Health Bars")]
         [Tooltip("Tableau de sliders (entre 1 et 4 éléments requis)")]
         public Slider[] enemySliders = new Slider[4];
+        public Slider[] PlayerSliders = new Slider[4];
         private int currentLifeValue;
         public EntityHandler entityHandler;
         
@@ -35,6 +36,11 @@ public class LifeEntity : MonoBehaviour
             {
                 enemySliders[i].maxValue = entityHandler.ennemies[i].UnitLife;
                 enemySliders[i].value = enemySliders[i].maxValue;
+            }
+            for (int i = 0; i < entityHandler.players.Length && i < PlayerSliders.Length; i++)
+            {
+                PlayerSliders[i].maxValue = entityHandler.players[i].UnitLife;
+                PlayerSliders[i].value = PlayerSliders[i].maxValue;
             }
         }
 }
