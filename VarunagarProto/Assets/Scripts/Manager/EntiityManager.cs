e4e4f8ddfe0c39292c3fe1e129bdd9d425795de5
SEO <seotoons@gmail.com>
2025-06-05 21:18:41 +0200

    je retry

--- VarunagarProto/Assets/Scripts/Manager/EntiityManager.cs
+++ VarunagarProto/Assets/Scripts/Manager/EntiityManager.cs
@@ -27,11 +27,12 @@ public class EntiityManager : MonoBehaviour
             var enemy = entityHandler.ennemies[i];
             if (enemy == null || enemy.UnitLife > 0)
                 continue;
-            int enemyIndex = i;
 
-            EffectsManager.SINGLETON.ClearEffectsForEntity(enemyIndex);
+            int visualIndex = enemy.index;
+            EffectsManager.SINGLETON.ClearEffectsForEntity(visualIndex);
+
+            Debug.Log($"L'ennemi {enemy.namE} est mort et va être désactivé.");
             CombatManager.SINGLETON.RemoveUnitFromList(enemy);
-
             if (i < CombatManager.SINGLETON.circlesEnnemy.Count)
             {
                 GameObject deadCircle = CombatManager.SINGLETON.circlesEnnemy[i];
@@ -42,27 +43,23 @@ public class EntiityManager : MonoBehaviour
                 }
             }
 
+            if (i < LifeEntity.SINGLETON.enemySliders.Length)
+            {
+                LifeEntity.SINGLETON.enemySliders[i].gameObject.SetActive(false);
+                LifeEntity.SINGLETON.enemyShieldSliders[i].gameObject.SetActive(false);
+                LifeEntity.SINGLETON.enemyPVTexts[i].gameObject.SetActive(false);
+
+            }
+
             if (enemy.instance != null)
             {
                 enemy.instance.SetActive(false);
                 enemy.UnitLife = -1;
             }
-            if (enemyIndex >= 0 && 
-                enemyIndex < LifeEntity.SINGLETON.enemySliders.Length && 
-                LifeEntity.SINGLETON.enemySliders[enemyIndex] != null)
-            {
-                Debug.Log($"Désactivation des sliders de l'ennemi à l'index {enemyIndex}");
-                LifeEntity.SINGLETON.enemySliders[enemyIndex].gameObject.SetActive(false);
-                LifeEntity.SINGLETON.enemyShieldSliders[enemyIndex].gameObject.SetActive(false);
-                LifeEntity.SINGLETON.enemyPVTexts[enemyIndex].gameObject.SetActive(false);
-            }
-            else
-            {
-                Debug.LogWarning($"Index {enemyIndex} hors limites ou sliders nuls.");
-            }
+            entityHandler.ennemies.RemoveAt(i);
+
         }
     }
-
     public void DestroyDeadPlayers()
     {
         for (int i = entityHandler.players.Count - 1; i >= 0; i--)
@@ -144,10 +141,10 @@ public class EntiityManager : MonoBehaviour
 
     void Update()
     {
-        if (entityHandler.ennemies.Any(e => e != null && e.UnitLife <= 0 && e.instance.activeSelf))
+        if (entityHandler.ennemies.Any(e => e != null && e.UnitLife <= 0))
             DestroyDeadEnemies();
 
-        if (entityHandler.players.Any(p => p != null && p.UnitLife <= 0 && p.instance.activeSelf))
+        if (entityHandler.players.Any(p => p != null && p.UnitLife <= 0))
             DestroyDeadPlayers();
 
         LifeEntity.SINGLETON.LifeManage();
